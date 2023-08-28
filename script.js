7461356996a70ae1609eeda6b1b735aa4789b2e9
MohamedAhmed2009 <142094136+MohamedAhmed2009@users.noreply.github.com>
2023-08-29 02:52:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let im =document.querySelector('.im') ;
+let container =document.querySelector('.container') ;
+function phone(phone){
+    im.src=phone;
+}
+function colors(color){
+    container.style.background=color;
+}
